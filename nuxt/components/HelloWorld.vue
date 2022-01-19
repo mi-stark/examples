41aceae1855da624dc0d626330cf27b704f51218
Aaron <*>
2022-01-19 10:42:41 +0800

    nuxt

---
+++ nuxt/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="title">this is a NUXT demo</div>
+    <el-button @click="handleClick">测试点击</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data(){
+    return {
+
+    }
+  },
+  methods: {
+    handleClick(e){
+      console.log(e)
+    }
+  },
+  created() {
+    console.log('created')
+  }
+}
+</script>
+
+<style scoped>
+.title{
+  color: blueviolet;
+}
+</style>
